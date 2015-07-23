92f09da017a6fdbdc2ed383bdb5f591eb987b6e4
Courtney Phillips & Ben Cornelis <student@epicodus.com>
2015-07-23 16:51:34 -0700

    change order new action and shopping cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,17 +2,20 @@ class OrdersController < ApplicationController
 
 
   def new
-    @order_publications = Publication.find(session[:cart])
-    @total = @order_publications.map(&:price).reduce(:+)
+    @order = Order.new
+    @order.add_items(session[:cart])
+    @order.save
+    # @order_publications = Publication.find(session[:cart].keys)
+    # @total = @order_publications.map(&:price).reduce(:+)
   end
 
   def create
     @order = Order.new(email: stripe_params[:stripeEmail], card_token: stripe_params[:stripeToken])
-    @order.add_items(publications_params)
+    @order.add_items(session[:cart])
     @order.calculate_total
     @order.process_payment
     @order.save
-    session[:cart] = []
+    session[:cart] = {}
     redirect_to order_path(@order)
   end
 
@@ -20,12 +23,12 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+  def update
+
+  end
+
   def update_cart
-    if session[:cart]
-      session[:cart] << params[:publication_id]
-    else
-      session[:cart] = [params[:publication_id]]
-    end
+    session[:cart][params[:publication_id]] = 1
 
     redirect_to publications_path, notice: "Item successfully added to cart."
   end
@@ -35,7 +38,8 @@ class OrdersController < ApplicationController
     params.permit(:stripeEmail, :stripeToken)
   end
 
-  def publications_params
-    params.select {|key, value| key.to_i.to_s == key }
-  end
+  # def publications_params
+  #   params.select {|key, value| key.to_i.to_s == key }
+  # end
+
 end
