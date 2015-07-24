eef0fb1f16b64650bf6208e7d4a337648b2c21d1
Jennifer <jbolanos707@gmail.com>
2015-07-23 17:14:11 -0700

    add main image on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,13 @@
 class PagesController < ApplicationController
 
-<<<<<<< HEAD
+
   def writers
-=======
+  end
 
   def index
   end
 
   def about
->>>>>>> b17ab0ce60687975ef01a322865a28bf9c1882ca
   end
 
-  def writers
-  end
 end
